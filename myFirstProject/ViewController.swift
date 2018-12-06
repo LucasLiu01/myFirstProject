3d61a8084d969ce3957b9607b749ac5005b03558
LucasLiu01 <s9865533@gmail.com>
2018-12-06 21:17:58 +0800

    第一版

---
+++ myFirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFirstProject
+//
+//  Created by 劉毅成 on 2018/12/6.
+//  Copyright © 2018年 劉毅成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
